d0dab2029989840a53c0faea35f366aadf796b83
Matthew <matthew.chiang.myc@gmail.com>
2021-05-03 05:58:08 -0400

    Setup Sqlite3

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+//sqlite database
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database(
+  "./database/fungible.db",
+  (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log("Connected to the database.");
+  }
+);
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
