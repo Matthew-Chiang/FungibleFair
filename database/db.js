2199794ebc8b490d09ed5273ed4972226ff47e4f
Matthew <matthew.chiang.myc@gmail.com>
2021-05-04 16:10:09 -0400

    added test for database connection

--- database/db.js
+++ database/db.js
@@ -3,34 +3,29 @@ const sqlite3 = require("sqlite3").verbose();
 
 let _db;
 
+function initDB(callBack) {
+  if (_db) {
+    return callBack(null, _db);
+  }
 
-function initDB(callBack){
-    if(_db){
-        return callBack(null,_db)
+  _db = new sqlite3.Database("./fungible2.db", (err) => {
+    if (err) {
+      callBack(err, null);
     }
-
-    _db = new sqlite3.Database(
-        "./fungible.db",
-        (err) => {
-          if (err) {
-            callBack(err,null)
-          }
-          console.log("Connected to the database.");
-          callBack(null)
-        }
-      );
+    console.log("Connected to the database.");
+    callBack(null);
+  });
 }
 
-function getDB(){
-    if(_db){
-        return _db;
-    }else{
-        return null
-    }
+function getDB() {
+  if (_db) {
+    return _db;
+  } else {
+    return null;
+  }
 }
-
 
 module.exports = {
-    getDB,
-    initDB
+  getDB,
+  initDB,
 };
