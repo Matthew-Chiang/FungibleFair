a642e13f78ea4a0ae7df27f34116ce4f52982149
Matthew <matthew.chiang.myc@gmail.com>
2021-05-04 05:34:31 -0400

    added database connection in a seperate file to persist connection

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var app = express();
+const app = express();
+
+const { getDB, initDB } = require("./database/db");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-//sqlite database
-var sqlite3 = require("sqlite3").verbose();
-var db = new sqlite3.Database(
-  "./database/fungible.db",
-  (err) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log("Connected to the database.");
+initDB((err) => {
+  if (err) {
+    console.log(err);
   }
-);
+});
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
