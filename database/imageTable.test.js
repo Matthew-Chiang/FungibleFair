812eef6d4f4dfef840f34faac8763e709ac8938e
Matthew <matthew.chiang.myc@gmail.com>
2021-05-06 15:51:52 -0400

    FIX: Testing helper for image creation is not writing to in memory database

--- database/imageTable.test.js
+++ database/imageTable.test.js
@@ -3,42 +3,41 @@ const Database = require("better-sqlite3");
 const TestingHelpers = require("../testing_helpers/db_testing_helpers");
 
 describe("Helper functions for Image Table", () => {
-    let testingDB;
+  let testingDB;
+  beforeEach(() => {
+    testingDB = new Database(":memory:", {});
+
+    TestingHelpers.createUserTable(testingDB);
+    TestingHelpers.createEmailIndex(testingDB);
+    TestingHelpers.createImageTable(testingDB);
+    TestingHelpers.createImageUserIDIndex(testingDB);
+
+    const userUUID = TestingHelpers.getUUID();
+    TestingHelpers.insertUser(testingDB, userUUID);
+  });
+
+  afterEach(() => {
+    testingDB.close();
+  });
+
+  test("Insert image", () => {
+    const info = imageTable.insertImage({
+      pathName: "/images/path",
+      isPublic: 1,
+      cost: 3.14,
+      userID: 1,
+      testingDB,
+    });
+
+    expect(info.changes).toEqual(1);
+  });
+
+  describe("Get image tests", () => {
     beforeEach(() => {
-        testingDB = new Database(":memory:", {});
-
-        TestingHelpers.createUserTable(testingDB);
-        TestingHelpers.createEmailIndex(testingDB);
-        TestingHelpers.createImageTable(testingDB);
-        TestingHelpers.createImageUserIDIndex(testingDB);
-
-        const userUUID = TestingHelpers.getUUID();
-        TestingHelpers.insertUser(testingDB, userUUID);
+      TestingHelpers.insertImage(testingDB);
     });
-
-    afterEach(() => {
-        testingDB.close();
+    test("Get image by userID", () => {
+      const image = imageTable.getImageByUserID({ userID: 1, testingDB });
     });
-
-    test("Insert image", () => {
-        const info = imageTable.insertImage({
-            pathName: "/images/path",
-            isPublic: 1,
-            cost: 3.14,
-            userID: 1,
-            testingDB,
-        });
-
-        expect(info.changes).toEqual(1);
-    });
-
-    describe("Get image tests", () => {
-        beforeEach(() => {
-            TestingHelpers.insertImage(testingDB);
-        });
-        test("Get image by userID", () => {
-            const image = imageTable.getImageByUserID({ userID: 1, testingDB });
-            console.log(image);
-        });
-    });
+  });
 });
