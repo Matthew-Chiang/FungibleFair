6d4942837223c4375986094ea53879e1ad76c0b6
Matthew <matthew.chiang.myc@gmail.com>
2021-05-07 00:32:42 -0400

    added route for creating a user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,41 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const crypto = require("crypto");
+const userTable = require("../database/userTable");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.put("/", function (req, res, next) {
+  // create user
+  const { name, email, password } = req.body;
+
+  const passwordConfig = {
+    hashBytes: 32,
+    saltBytes: 16,
+    iterations: 100000,
+  };
+
+  const salt = crypto.randomBytes(passwordConfig.saltBytes).toString("base64");
+
+  const hashedPassword = crypto
+    .pbkdf2Sync(
+      password,
+      salt,
+      passwordConfig.iterations,
+      passwordConfig.hashBytes,
+      "SHA512"
+    )
+    .toString("base64");
+  let info;
+  try {
+    info = userTable.insertUser({
+      name,
+      email,
+      password,
+    });
+  } catch (err) {
+    throw err;
+  }
+
+  res.json({ info });
 });
 
 module.exports = router;
