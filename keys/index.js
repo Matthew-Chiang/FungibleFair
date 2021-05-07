0ef3bc76ea79585c15ea5434aa6b58e421a026a6
Matthew <matthew.chiang.myc@gmail.com>
2021-05-07 03:22:31 -0400

    added jwt support on login

---
+++ keys/index.js
@@ -0,0 +1,6 @@
+// this is unsafe but I wanted for this to run out of the box
+// we should be probably be in an environment variable
+
+module.exports = {
+  ACCESS_TOKEN_SECRET: "sCU3MgvQhaVfKevd",
+};
